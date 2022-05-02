6727def47d5c89524cd79fbc034d0d6eda768b81
User <User@DESKTOP-OFTTR4B>
2022-05-02 16:37:37 -0300

    Adicionar arquivos de projeto.

---
+++ FatorialRecursivo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialRecursivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular o Fatorial de qual número? ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("O fatorial do " + num + " é: " + Fatorial(num));
+
+            Console.ReadKey();
+        }
+
+        static int Fatorial (int num)
+        {
+            if (num <= 0)
+            {
+                return 1;
+            }
+
+            return num * Fatorial(num - 1);
+        }
+    }
+}
